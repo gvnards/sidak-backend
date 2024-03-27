f8f731642a56b53608d50955f2ec4cb6237ef531
gvnards <gvn.ardiansyah@gmail.com>
2024-03-27 22:47:30 +0700

    updating get list pegawai, menambahkan nama unor lengkap dan meringkaskan get data dari DB agar tidak banyak akses ke DB

--- app/Http/Controllers/ListPegawaiController.php
+++ app/Http/Controllers/ListPegawaiController.php
@@ -18,41 +18,76 @@ class ListPegawaiController extends Controller
     $message = $this->decrypt('sidak.bkpsdmsitubondokab', $request->header('Authorization'));
     $message = json_decode($message, true);
     $idAppRoleUser = $message['idAppRoleUser'];
-    $kodeKomponenAdmin = '';
-    $kdKom = DB::table('m_admin')->where('username', '=', $username)->get();
-    foreach(json_decode($kdKom, true) as $key => $value) {
-      $kodeKomponenAdmin = $kodeKomponenAdmin.$value['unitOrganisasi'].'%';
-    }
-    if ($idAppRoleUser === 1) {
-      $data = DB::table('v_short_brief')->groupBy('id')->get([
-        'id as id',
-        'nama as nama',
-        'nip as nip',
-        'jabatan as jabatan',
-        'unitOrganisasi as unitOrganisasi',
-        'golongan as golongan',
-        'pangkat as pangkat'
-      ]);
-    } else {
-      $data = DB::table('v_short_brief')->where('kodeKomponen', 'LIKE', $kodeKomponenAdmin)->groupBy('id')->get([
-        'id as id',
-        'nama as nama',
-        'nip as nip',
-        'jabatan as jabatan',
-        'unitOrganisasi as unitOrganisasi',
-        'golongan as golongan',
-        'pangkat as pangkat'
-      ]);
-    }
-    $namaUnitOrganisasi = json_decode(DB::table('m_admin')->join('m_unit_organisasi', 'm_admin.unitOrganisasi', '=', 'm_unit_organisasi.kodeKomponen')->where([
+    $userAdmin = json_decode(DB::table('m_admin')->join('m_unit_organisasi', 'm_admin.unitOrganisasi', '=', 'm_unit_organisasi.kodeKomponen')->where([
       ['m_admin.username', '=', $username]
     ])->get([
-      'm_unit_organisasi.nama as unitOrganisasi'
+      'm_unit_organisasi.kodeKomponen as kodeKomponen',
+      'm_unit_organisasi.nama as namaUnitOrganisasi'
     ]), true);
+    if ($idAppRoleUser === 1) {
+      $data = json_decode(DB::table('v_short_brief')->groupBy('id')->get([
+        'id as id',
+        'nama as nama',
+        'nip as nip',
+        'jabatan as jabatan',
+        'kodeKomponen as kodeKomponen',
+        'unitOrganisasi as unitOrganisasi',
+        'golongan as golongan',
+        'pangkat as pangkat'
+      ]), true);
+    } else {
+      $data = json_decode(DB::table('v_short_brief')->where('kodeKomponen', 'LIKE', $userAdmin[0]['kodeKomponen'])->groupBy('id')->get([
+        'id as id',
+        'nama as nama',
+        'nip as nip',
+        'jabatan as jabatan',
+        'kodeKomponen as kodeKomponen',
+        'unitOrganisasi as unitOrganisasi',
+        'golongan as golongan',
+        'pangkat as pangkat'
+      ]), true);
+    }
+    $listKodeKomponen = [];
+    foreach ($data as $value) {
+      $kdExplode = explode(".", $value['kodeKomponen']);
+      $countKdExplode = count($kdExplode);
+      for ($i = 0; $i < $countKdExplode; $i++) {
+        if (count($listKodeKomponen) === 0) {
+          array_push($listKodeKomponen, implode(".", $kdExplode));
+        } else {
+          if (count($kdExplode) === 0) break;
+          $kdImplode = implode(".", $kdExplode);
+          $isHasKd = false;
+          foreach ($listKodeKomponen as $listKd) {
+            if ($kdImplode === $listKd) $isHasKd = true;
+          }
+          if (!$isHasKd) {
+            array_push($listKodeKomponen, $kdImplode);
+          }
+          array_pop($kdExplode);
+        }
+      }
+    }
+    $allUnors = (new DataJabatanController)->getAllUnor(DB::table('m_unit_organisasi')->whereIn('kodeKomponen', $listKodeKomponen)->where([
+      ['idBkn', '!=', ''],
+      ['kodeKomponen', 'NOT LIKE', '-%']
+    ])->get());
+    for ($i = 0; $i < count($data); $i++) {
+      if (str_contains($data[$i]['kodeKomponen'], "-")) {
+        $data[$i]['unitOrganisasi'] = "(Unit organisasi tidak ada di dalam database. Silahkan update atau konsultasi dengan BKPSDM.)";
+        continue;
+      }
+      for ($j = 0; $j < count($allUnors); $j++) {
+        if ($data[$i]['kodeKomponen'] === $allUnors[$j]['kodeKomponen']) {
+          $data[$i]['unitOrganisasi'] = $allUnors[$j]['nama'];
+          break;
+        }
+      }
+    }
     $callback = [
       'message' => [
         'pegawai' => $data,
-        'unitOrganisasi' => $namaUnitOrganisasi[0]['unitOrganisasi']
+        'unitOrganisasi' => $userAdmin[0]['namaUnitOrganisasi']
       ],
       'status' => 2
     ];
